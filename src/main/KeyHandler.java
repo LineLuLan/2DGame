9d8e2f8278ba5c1d46ed4fa47d39a0bb0671a7e4
Nguyen Duc Hai <ngduchaidev0301@gmail.com>
2025-05-22 09:07:53 +0700

    edit teleport

--- src/main/KeyHandler.java
+++ src/main/KeyHandler.java
@@ -299,6 +299,7 @@ public class KeyHandler implements KeyListener{
                 gp.playMusic(0);
             }
             else if (gp.ui.commandNum == 1){
+                gp.ui.titleScreenState = 0;
                 gp.gameState = gp.titleState;
                 gp.restart();
             }
